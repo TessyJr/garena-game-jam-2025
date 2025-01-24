371fb7127d553e8285ae9780c26aa622922621d5
vinchen <vinchen996@gmail.com>
2025-01-24 23:54:14 +0700

    feat: level 2

--- Assets/Scripts/Objects/ConveyorComponent.cs
+++ Assets/Scripts/Objects/ConveyorComponent.cs
@@ -8,7 +8,7 @@ public class ConveyorComponent : MonoBehaviour
     [SerializeField] private ForceMode2D forceMode = ForceMode2D.Force; 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("ButtonObject"))
         {
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
             if (rb != null)
