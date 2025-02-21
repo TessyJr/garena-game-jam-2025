aeedce948ff911c70d12576e12b46dc520573c77
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2025-02-21 09:49:59 +0700

    fix: conveyors

--- Assets/Scripts/Objects/ConveyorComponent.cs
+++ Assets/Scripts/Objects/ConveyorComponent.cs
@@ -5,11 +5,24 @@ public class ConveyorComponent : MonoBehaviour
     [SerializeField] private Vector2 direction = Vector2.right;
     [SerializeField] private float speed = 2f;
     [SerializeField] private ForceMode2D forceMode = ForceMode2D.Force;
-    private void OnCollisionStay2D(Collision2D collision)
+
+    // private void OnCollisionStay2D(Collision2D collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("ButtonObject"))
+    //     {
+    //         if (collision.gameObject.TryGetComponent<Rigidbody2D>(out var rb))
+    //         {
+    //             Vector2 conveyorForce = direction.normalized * speed;
+    //             rb.AddForce(conveyorForce, forceMode);
+    //         }
+    //     }
+    // }
+
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("ButtonObject"))
+        if (other.CompareTag("Player") || other.CompareTag("ButtonObject"))
         {
-            if (collision.gameObject.TryGetComponent<Rigidbody2D>(out var rb))
+            if (other.TryGetComponent<Rigidbody2D>(out var rb))
             {
                 Vector2 conveyorForce = direction.normalized * speed;
                 rb.AddForce(conveyorForce, forceMode);
